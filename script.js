eb2f837c525e859c82f539c2cf66b14217aec534
Melekkorkmazz <mk1244657@gmail.com>
2026-02-20 12:17:30 +0300

    js eklendi

---
+++ script.js
@@ -0,0 +1,18 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value;
+
+    if (taskText === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    // Tıklayınca sil
+    li.onclick = function () {
+        li.remove();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
